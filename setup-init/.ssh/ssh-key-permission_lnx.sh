bb3fdf88f00b4ba9dbc64a7bc685028d3cd2f7fa
de1chk1nd <de1chk1nd@outlook.com>
2024-08-25 15:07:03 +0200

    modify for linux change source-destination check to AWS CLI

--- setup-init/.ssh/ssh-key-permission_lnx.sh
+++ setup-init/.ssh/ssh-key-permission_lnx.sh
@@ -1,3 +1,5 @@
 #/bin/bash
 
 chmod 600 ./setup-init/.ssh/de1chk1nd-ssh.pem
+gnome-terminal -e 'ssh -i ./setup-init/.ssh/de1chk1nd-ssh.pem sudo ./setup-init/.ssh/ssh-key-permission_lnx.sh ubuntu@ubuntu-eu-central-1.de1chk1nd-lab.aws'
+gnome-terminal -e 'ssh -i ./setup-init/.ssh/de1chk1nd-ssh.pem sudo ./setup-init/.ssh/ssh-key-permission_lnx.sh ubuntu@ubuntu-eu-west-1.de1chk1nd-lab.aws'
