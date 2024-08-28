4fcfff1fe15b13fba8fdd5ee278f91bb36a50b15
de1chk1nd <de1chk1nd@outlook.com>
2024-08-28 23:59:00 +0200

    change CIDR - clear separation

--- infrastructure/modules/regions/nacl.tf
+++ infrastructure/modules/regions/nacl.tf
@@ -15,6 +15,22 @@ resource "aws_network_acl" "main" {
     protocol   = -1
     rule_no    = 101
     action     = "allow"
+    cidr_block = "172.16.0.0/12"
+    from_port  = 0
+    to_port    = 0
+  }
+  ingress {
+    protocol   = -1
+    rule_no    = 102
+    action     = "allow"
+    cidr_block = "192.168.0.0/16"
+    from_port  = 0
+    to_port    = 0
+  }
+  ingress {
+    protocol   = -1
+    rule_no    = 110
+    action     = "allow"
     cidr_block = "0.0.0.0/0"
     #cidr_block = var.student_ip
     from_port = 0
