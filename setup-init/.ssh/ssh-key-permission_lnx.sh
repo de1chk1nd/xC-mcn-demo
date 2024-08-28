9ed7816d269b25a58f57f66c249227db6f3444d0
de1chk1nd <de1chk1nd@outlook.com>
2024-08-28 23:06:17 +0200

    keepalive for ssh

--- setup-init/.ssh/ssh-key-permission_lnx.sh
+++ setup-init/.ssh/ssh-key-permission_lnx.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
 chmod 600 ./setup-init/.ssh/de1chk1nd-ssh.pem
-x-terminal-emulator -e '/usr/bin/ssh -o StrictHostKeyChecking=no -i /home/de1chk1nd/Documents/git-repositories/xC-mcn-demo/setup-init/.ssh/de1chk1nd-ssh.pem ubuntu@ubuntu-eu-central-1.de1chk1nd-lab.aws' &
-x-terminal-emulator -e '/usr/bin/ssh -o StrictHostKeyChecking=no -i /home/de1chk1nd/Documents/git-repositories/xC-mcn-demo/setup-init/.ssh/de1chk1nd-ssh.pem ubuntu@ubuntu-eu-west-1.de1chk1nd-lab.aws' &
+x-terminal-emulator -e '/usr/bin/ssh -o ServerAliveInterval=180 -o ServerAliveCountMax=2 -o StrictHostKeyChecking=no -i /home/de1chk1nd/Documents/git-repositories/xC-mcn-demo/setup-init/.ssh/de1chk1nd-ssh.pem ubuntu@ubuntu-eu-central-1.de1chk1nd-lab.aws' &
+x-terminal-emulator -e '/usr/bin/ssh -o ServerAliveInterval=180 -o ServerAliveCountMax=2 -o StrictHostKeyChecking=no -i /home/de1chk1nd/Documents/git-repositories/xC-mcn-demo/setup-init/.ssh/de1chk1nd-ssh.pem ubuntu@ubuntu-eu-west-1.de1chk1nd-lab.aws' &
