6310f1e12278a9aa3a4166b2aa0ec8e7349bcd76
de1chk1nd <de1chk1nd@outlook.com>
2024-08-28 23:59:00 +0200

    change CIDR - clear separation

--- infrastructure/site_module.tf
+++ infrastructure/site_module.tf
@@ -4,10 +4,10 @@ module "eu-central-1" {
   region       = local.setup-init.aws.region_site_1
   auth_profile = local.setup-init.aws.auth_profile
 
-  vpc_cidr    = "10.1.0.0/16"
-  subnet_cidr = "10.1.0.0/24"
+  vpc_cidr    = "10.0.0.0/16"
+  subnet_cidr = "10.0.0.0/24"
   ubuntu_ami  = lookup(var.ubuntu_ami, local.setup-init.aws.region_site_1)
-  remote_cidr = "10.10.0.0/16"
+  remote_cidr = "172.16.0.0/16"
 
   public_key = tls_private_key.ssh_key_access.public_key_openssh
 
@@ -23,10 +23,10 @@ module "eu-west-1" {
   region       = local.setup-init.aws.region_site_2
   auth_profile = local.setup-init.aws.auth_profile
 
-  vpc_cidr    = "10.10.0.0/16"
-  subnet_cidr = "10.10.0.0/24"
+  vpc_cidr    = "172.16.0.0/16"
+  subnet_cidr = "172.16.0.0/24"
   ubuntu_ami  = lookup(var.ubuntu_ami, local.setup-init.aws.region_site_2)
-  remote_cidr = "10.1.0.0/16"
+  remote_cidr = "10.0.0.0/16"
 
   public_key = tls_private_key.ssh_key_access.public_key_openssh
 
