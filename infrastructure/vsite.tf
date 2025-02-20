abc193b91e27ff97dd28e1a195d62dc52ae21171
de1chk1nd <de1chk1nd@outlook.com>
2025-02-20 15:04:04 +0100

    bigip bug fixing

--- infrastructure/vsite.tf
+++ infrastructure/vsite.tf
@@ -1,14 +1,14 @@
-# resource "volterra_virtual_site" "vk8s_sites" {
-#  name      = "de1chk1nd-vk8s-sites"
-#  namespace = "system"
-# 
-#  site_selector {
-#    expressions = ["de1chk1nd-vk8s-sites in (basic-demo)"]
-#  }
-# 
-#  site_type = "CE"
-# 
-#  depends_on = [
-#    volterra_known_label.vk8s_sites
-#  ]
-# 
+resource "volterra_virtual_site" "vk8s_sites" {
+  name      = "de1chk1nd-vk8s-sites"
+  namespace = "shared"
+
+  site_selector {
+    expressions = ["de1chk1nd-mcn-sites in (vk8s)"]
+  }
+
+  site_type = "CUSTOMER_EDGE"
+
+  depends_on = [
+    volterra_known_label.vk8s_sites
+  ]
+} 
