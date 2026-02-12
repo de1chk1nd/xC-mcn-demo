a79f560ee69af8aed8c29357460963693ee11913
de1chk1nd <de1chk1nd@outlook.com>
2026-02-12 20:35:08 +0100

    provider

--- xC-use-cases/vk8s/terraform/provider.tf
+++ xC-use-cases/vk8s/terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "volterra" {
-  api_p12_file = "/home/de1chk1nd/Documents/git-repositories/xC-mcn-demo/setup-init/.xC/f5-emea-ent.console.ves.volterra.io.api-creds.p12"
+  api_p12_file = "../../../setup-init/.xC/f5-emea-ent.console.ves.volterra.io.api-creds.p12"
   url          = local.setup-init.xC.tenant_api
 }
 
